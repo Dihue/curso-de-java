ea9c020bbb5a9c782ebf6089192275bd92671503
Dihue <dihue21@gmail.com>
2022-08-12 12:00:03 -0300

    Primer commit en clase

--- src/Laboratorio/En_Clase/Ejer01_HolaMundo.java
+++ src/Laboratorio/En_Clase/Ejer01_HolaMundo.java
@@ -1,2 +1,12 @@
-package Laboratorio.En_Clase;public class Ejer01_HolaMundo {
+package Laboratorio.En_Clase;
+
+public class Ejer01_HolaMundo {
+
+	public static void main(String[] args) {
+
+		String texto = "Hola Mundo";
+		System.out.println(texto);
+
+	}
+
 }
