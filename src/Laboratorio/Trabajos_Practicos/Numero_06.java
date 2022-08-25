e1f8e494aa961c9020df5710df5bcde6c60a5980
Dihue <dihue21@gmail.com>
2022-08-25 00:46:56 -0300

    Trabajo Practico Numero 6

---
+++ src/Laboratorio/Trabajos_Practicos/Numero_06.java
@@ -0,0 +1,38 @@
+package Laboratorio.Trabajos_Practicos;
+
+public class Numero_06 {
+
+	public static void main(String[] args) {
+
+		double A = 2.31;
+		double B = 1.23;
+
+		double seno = Math.sin(A);
+		double coseno = Math.cos(A);
+		double tangente = Math.tan(A);
+
+		double atangente = Math.atan(A);
+		double atangente2 = Math.atan2(A, B);
+
+		double exponencial = Math.exp(B);
+		double logaritmo = Math.log(B);
+
+		double numeroPi = Math.PI;
+		double numeroE = Math.E;
+
+
+		System.out.println(seno);
+		System.out.println(coseno);
+		System.out.println(tangente);
+
+		System.out.println(atangente);
+		System.out.println(atangente2);
+
+		System.out.println(exponencial);
+		System.out.println(logaritmo);
+
+		System.out.println(numeroPi);
+		System.out.println(numeroE);
+	}
+
+}
